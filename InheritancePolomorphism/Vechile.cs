a4e246af73f2f84c2bdfa86ff10305c5c5933cb1
mjthomas22 <mthomas22@gmail.com>
2017-07-13 10:29:11 -0400

    Added Vechile Class

---
+++ InheritancePolomorphism/Vechile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolomorphism
+{
+    class Vechile
+    {
+        //Fields
+        protected int seats;
+        protected int carryingCapacity;
+        protected string color;
+        protected double movementSpeed;
+        protected double distanceTraveled;
+
+        private float gearRatio;
+        private float engineSize;
+
+
+        //Properties
+
+        public int Seats
+        { get; }
+
+        public int CarryingCapacity
+        { get; }
+
+        public string Color
+        { get; set; }
+
+        public double DistanceTraveled
+        { get; set; }
+
+        //Constructors
+
+        public Vechile()
+        {
+
+        }
+        public Vechile(int seats, int carryingCapactiy, string color, double movementSpeed)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapactiy;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+
+            distanceTraveled = 0.0d;
+        }
+        public void Move()
+        {
+            distanceTraveled += movementSpeed;
+        }
+
+        public string GetDistanceTravled()
+        {
+            return "Distance Traveled:" + distanceTraveled;
+        }
+
+    }
+
+
+
+
+}
+
